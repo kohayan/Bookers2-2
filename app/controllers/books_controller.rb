220b98fad1559a19e5e20441a286a0a114230fc2
kohayan <disney.2381509@gmail.com>
2020-03-07 09:21:46 +0000

    [Finish] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,12 +32,15 @@ class BooksController < ApplicationController
 
     def edit
     	@book = Book.find(params[:id])
+    	if @book.user.id != current_user.id
+    		redirect_to books_path
+    	end
     end
 
     def update
     	@book = Book.find(params[:id])
     	if @book.update(book_params)
-    		redirect_to book_path(book), notice:"Book was successfully updated."
+    		redirect_to book_path(@book), notice:"Book was successfully updated."
     	else
     		render :edit
     	end
